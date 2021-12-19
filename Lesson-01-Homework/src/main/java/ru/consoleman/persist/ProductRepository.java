d7b3246bced110c7ecfca882badbb9f3468ae66c
consoleman <consoleman108@gmail.com>
2021-12-19 22:32:21 +0300

    Initial commit

---
+++ Lesson-01-Homework/src/main/java/ru/consoleman/persist/ProductRepository.java
@@ -0,0 +1,14 @@
+package ru.consoleman.persist;
+
+import java.util.List;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    Product findById(long id);
+
+    void save(Product product);
+
+    void delete(long id);
+}
